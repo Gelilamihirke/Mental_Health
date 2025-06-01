5f76ed2751ea29160e0a734b375e3d0be9115b4d
Sumeya Awel <145346271+Sumiislamic1@users.noreply.github.com>
2025-06-01 13:52:22 +0300

    Update main.js
    js updated rock

--- rock_paper_scissor/js/main.js
+++ rock_paper_scissor/js/main.js
@@ -43,3 +43,49 @@ const userWon = () => {
   userScoreVal.innerText = Number(userScoreVal.innerText) + 1;
   return "User Won";
 };
+
+const findAnswer = (userChoice, computerChoice) => {
+  if (
+    (userChoice === "r" && computerChoice === "r") ||
+    (userChoice === "p" && computerChoice === "p") ||
+    (userChoice === "s" && computerChoice === "s")
+  ) {
+    return "Match Draw";
+  }
+  if (userChoice === "r" && computerChoice === "p") {
+    return computerWon();
+  } else if (userChoice === "r" && computerChoice === "s") {
+    return userWon();
+  } else if (userChoice === "p" && computerChoice === "s") {
+    return computerWon();
+  } else if (userChoice === "p" && computerChoice === "r") {
+    return userWon();
+  } else if (userChoice === "s" && computerChoice === "r") {
+    return computerWon();
+  } else if (userChoice === "s" && computerChoice === "p") {
+    return userWon();
+  }
+};
+
+choice.forEach((choiceElement) => {
+  choiceElement.addEventListener("click", () => {
+    const answerElement = document.getElementById("answer");
+    if (answerElement) {
+      answerElement.remove();
+    }
+
+    computerChoice();
+    console.log("Computer choice : " + compChoice);
+
+    let userMsg;
+    userChoice = choiceElement.getAttribute("id");
+    if (userChoice == "r") {
+      userMsg = "User: Rock";
+    } else if (userChoice == "p") {
+      userMsg = "User: Paper";
+    } else if (userChoice == "s") {
+      userMsg = "User: Scissor";
+    }
+    resultUserStat.innerHTML = userMsg;
+
+    
