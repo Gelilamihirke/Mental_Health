5bd70906a53ee9b1207fff10642545424884e421
Sumeya Awel <145346271+Sumiislamic1@users.noreply.github.com>
2025-06-02 09:23:24 +0300

    Update style.js
    js for watch

--- StopWatch/style.js
+++ StopWatch/style.js
@@ -9,11 +9,30 @@ window.onload = function() {
     // Declare variable 'seconds' and assign connection to the HTML element with the specified ID
     var seconds = 00; 
 
-    // Declare variable 'milliseconds' and assign connection to the HTML element with the specified ID
+    
     var milliseconds = 00; 	
 
-    // Declare variables to attach hours, minutes, seconds, and milliseconds, and assign connections to the corresponding HTML elements
+    
     var attachHours = document.getElementById("hours");
     var attachMinutes = document.getElementById("minutes");
     var attachSeconds = document.getElementById("seconds");
     var attachMilliseconds = document.getElementById("milliseconds");
+
+    var startButton = document.getElementById('start-button'); 
+    var stopButton = document.getElementById('stop-button');
+    var resetButton = document.getElementById('reset-button');
+
+    
+    var interval;
+
+
+    startButton.onclick = function() {
+       
+       interval = setInterval(countDuration, 10);
+    }
+
+    
+    stopButton.onclick = function() {
+     
+        clearInterval(interval);
+    }
