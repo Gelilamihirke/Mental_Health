609564ed8b15f853e299b647f056945f08f94ea6
Sumeya Awel <145346271+Sumiislamic1@users.noreply.github.com>
2025-06-02 09:21:13 +0300

    Create style.js
    fist commit on js for stopwatch

---
+++ StopWatch/style.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  
+    // Declare variable 'hours' and assign connection to the HTML element with the specified ID
+    var hours = 00; 
+
+    // Declare variable 'minutes' and assign connection to the HTML element with the specified ID
+    var minutes = 00;
+
+    // Declare variable 'seconds' and assign connection to the HTML element with the specified ID
+    var seconds = 00; 
+
+    // Declare variable 'milliseconds' and assign connection to the HTML element with the specified ID
+    var milliseconds = 00; 	
+
+    // Declare variables to attach hours, minutes, seconds, and milliseconds, and assign connections to the corresponding HTML elements
+    var attachHours = document.getElementById("hours");
+    var attachMinutes = document.getElementById("minutes");
+    var attachSeconds = document.getElementById("seconds");
+    var attachMilliseconds = document.getElementById("milliseconds");
