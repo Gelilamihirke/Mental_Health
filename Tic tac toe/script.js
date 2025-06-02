c969f620fde69c142620e466d4fcc500ebb26780
Sumeya Awel <145346271+Sumiislamic1@users.noreply.github.com>
2025-06-02 08:11:39 +0300

    Update script.js
    java script for tic

--- Tic tac toe/script.js
+++ Tic tac toe/script.js
@@ -37,3 +37,20 @@ const checkWin = ()=>{
         }
     })
 }
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+            } 
+        }
+    })
+})
